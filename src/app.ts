27048468cb394fb041c2a12e22e36c8f2227f3d2
= <=>
2024-03-28 22:37:11 +0530

    remain to solve socketio i issue

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express , {Express , Request, Response} from 'express'
+import http from 'http'
+import {Server} from 'socket.io'
+import path from 'path'
+
 
 const app:Express=express();
 const PORT:number =3000
 
+const server = http.createServer(app)
+app.use(express.static('./static'))
+
+const ioServer=new Server(server)
+
 app.get('/',function(req:Request,res:Response){
-    res.send('hello from express')
-    return  
+    res.sendFile(path.resolve()+'/static/index.js')
+    
 })
 
 
-app.listen(PORT,()=>console.log('server is up and running ...'))
+server.listen(PORT,()=>console.log('server is up and running ...'))
+
